03a9d6bd087214934a28bb6f332b7eafe8a715bb
wangjiajia <wangjiajia@b.360.cn>
2017-11-20 20:52:16 +0800

    add main code

--- data-input-app/src/main.js
+++ data-input-app/src/main.js
@@ -1,7 +1,31 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueRouter from "vue-router";
+
+Vue.use(VueRouter);
+
+Vue.config.debug = true;
+const First = { template: '<div><h2>我是第 1 个子页面</h2></div>' }
+const Second = { template: '<div><h2>我是第 2 个子页面</h2></div>' }
+
+const router = new VueRouter({
+  mode: 'history',
+  base: __dirname,
+  routes: [
+    {
+      path: '/first',
+      component: First
+    },
+    {
+      path: '/second',
+      component: Second
+    }
+  ]
+})
+
 
 new Vue({
   el: '#app',
+  router: router,
   render: h => h(App)
 })
