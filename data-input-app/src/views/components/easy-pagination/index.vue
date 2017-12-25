f477dd62697d3729c52148e7aa752da55212f0b1
wangjiajia <wangjiajia@A003216-pc1.ESG.360ES.CN>
2017-12-25 19:08:05 +0800

    增加esay-pagination

---
+++ data-input-app/src/views/components/easy-pagination/index.vue
@@ -0,0 +1,80 @@
+<template lang="html">
+  <div class="easy-pagination">
+    <slot :data="currentItems">
+    </slot>
+    <Pagination
+      layout="prev, pager, next"
+      :page-count="pageCount"
+      :current-page="currentPage"
+      @current-change="currentChange">
+    </Pagination>
+  </div>
+</template>
+
+<script>
+import Vue from 'vue';
+
+// TODO: 修改页码时没锁定，异步请求，可能数据与当前页码不一致不一致
+
+export default Vue.component('easy-pagination', {
+  props: ['data'],
+  mounted () {
+  },
+  data () {
+    return {
+      items: [],
+      currentItems: [],
+      offset: 0,
+      limit: 10,
+      total: 0,
+      currentPage: 1,
+      pageSize: 10
+    };
+  },
+  methods: {
+    getCurrentPageTableData () {
+      console.log('getCurrentPageTableData', this.pageRangeMin);
+      let start = Math.abs(this.currentPage - this.pageRangeMin) * this.pageSize;
+      let end = start + this.pageSize;
+
+      if (Array.isArray(this.items)) {
+        return this.items.slice(start, end);
+      }
+    },
+    currentChange (currentPage) {
+      this.currentPage = currentPage;
+      if (((currentPage < this.pageRangeMin) && (currentPage >= 1)) || ((currentPage > this.pageRangeMax) && (currentPage <= this.pageCount))) {
+        this.offset = (currentPage - 1) * this.pageSize;
+        this.$emit('get-data', {offset: this.offset, limit: this.limit});
+      }
+    }
+  },
+  computed: {
+    pageCount () {
+      return Math.ceil(this.total / this.pageSize);
+    },
+    pageRangeMax () {
+      return Math.floor((this.offset + this.limit) / this.pageSize);
+    },
+    pageRangeMin () {
+      return Math.ceil(this.offset / this.pageSize) + 1;
+    }
+  },
+  watch: {
+    currentPage (val, oldVal) {
+      this.currentItems = this.getCurrentPageTableData();
+    },
+    data (val, oldVal) {
+      this.items = val.items;
+      this.total = val.total;
+      this.currentItems = this.getCurrentPageTableData();
+    },
+    currentItems (val, oldVal) {
+      console.log(val);
+    }
+  }
+});
+</script>
+
+<style lang="less">
+</style>
