481d61f2143772585ecde796e5ca93a4825035f1
Jiajia Wang <jiajiawang@MacBook-Pro-4.local>
2018-12-20 17:57:17 +0800

    生产环境不使用mock测试数据

--- data-input-app/src/main.js
+++ data-input-app/src/main.js
@@ -4,8 +4,9 @@ import VueRouter from 'vue-router'
 import Element from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 import routerConf from './config/routerConf'
-require('../test');
-
+if (process.env.NODE_ENV != 'production') {
+  require('../test');
+}
 Vue.use(VueRouter);
 Vue.use(Element);
 
